9c0a9ff536dc0360391a7ade22443bbdc3bf4d73
weiqiang.Yang <scutweiqiang.yang@gmail.com>
2013-04-13 10:23:23 +0800

    csv file manipulation

--- demos/csvfile.py
+++ demos/csvfile.py
@@ -6,26 +6,33 @@ __author__ = 'vivi'
 """
 this is a simple code snipet manipulating csv file
 csv = comma separated values
+http://en.wikipedia.org/wiki/Comma-separated_values
+一般来说，csv文件每行一条记录，一般来说每条记录的field的数量是相同的
+field之间使用delimiter来分割，如果field里面有delemiter，field需要使用quotechar包起来，field里面的quotechar使用两个quote表示
 """
 
 import csv
 
-def write_csv_file(rows, filepath='file.csv'):
+def write_csv_file(rows, filepath='file-out.csv'):
     """
     write the data into a csv file.
     """
     with open(filepath, 'wb') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerow(rows)
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in rows:
+            csvwriter.writerow(row)
 
 def read_csv_file(filepath='file.csv'):
     """
     reads the csv file and process
     """
+    rows = []
     with open(filepath, 'rb') as f:
-        csvreader = csv.reader(f, delimiter=',') # reader 可以接收一个file-like-obj
+        csvreader = csv.reader(f, delimiter=',', quotechar='"') # reader 可以接收一个file-like-obj
         for row in csvreader:
+            rows.append(row)
             print '-'.join(row)
+    return rows
 
 def usewith():
     with with_file() as f:
@@ -45,7 +52,8 @@ class with_file:
 
 if __name__ == '__main__':
     usewith()
-    rows = [x for x in xrange(1,10,1)]
+#    rows = [x for x in xrange(1,10,1)]
+#    write_csv_file(rows)
+    rows = read_csv_file('file.csv')
     write_csv_file(rows)
-    read_csv_file('file.csv')
 
